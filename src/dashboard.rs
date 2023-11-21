886b7b02dcb0620f34c23b7eed985abb119b2e6b
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2023-11-21 20:30:02 +0545

    Output the battery history to a .csv file for using in graphing (temporary).

--- src/dashboard.rs
+++ src/dashboard.rs
@@ -1,18 +1,27 @@
+use glib::Object;
 use gtk::glib::subclass::InitializingObject;
+use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 use gtk::{glib, CompositeTemplate};
 
-use glib::Object;
+use std::fs;
+use std::io::prelude::*;
+use std::path::Path;
+
+use crate::utils::load_historic_data::load_data;
 
 mod imp {
+
+    use gtk::glib::{log_structured, LogLevel};
+
+    use crate::utils::load_historic_data::ChargeState;
+
     use super::*;
 
     // Object holding the state
     #[derive(CompositeTemplate, Default)]
     #[template(resource = "/com/prophesy/ui/prophesy_dashboard.ui")]
-    pub struct Dashboard {
-
-    }
+    pub struct Dashboard {}
 
     #[glib::object_subclass]
     impl ObjectSubclass for Dashboard {
@@ -40,8 +49,49 @@ mod imp {
             self.parent_constructed();
 
             // Load latest window state
-            let obj = self.obj();
+            let _ = self.obj();
 
+            // loading the data
+            let battery_history_data = load_data();
+            for (f, d) in battery_history_data.iter() {
+                let mut file_basename = f
+                    .basename()
+                    .expect("Couldn't get file basename for battery report file.");
+                file_basename.set_extension(".csv");
+                
+                let file_name = file_basename.file_name().expect("Couldn't get filename for battery history file");
+
+                let battery_history_file_path = Path::new(file_name);
+
+                let mut battery_history_file = fs::File::create(battery_history_file_path)
+                    .expect("Couldn't create battery_history_file");
+
+                for (dt, dv) in d.iter() {
+                    battery_history_file
+                        .write_fmt(format_args!(
+                            "{},{},{}\n",
+                            dt.to_unix(),
+                            dv.value,
+                            match dv.charge_state {
+                                ChargeState::Charging => {
+                                    "Charging"
+                                }
+                                ChargeState::Discharging => {
+                                    "Discharging"
+                                }
+                                ChargeState::Unknown => {
+                                    "Unknown"
+                                }
+                            }
+                        ))
+                        .unwrap();
+                }
+                log_structured!("Prophesy",
+                LogLevel::Debug,
+                {
+                    "MESSAGE" => std::format!("Battery history saved to {:?}", battery_history_file_path.as_os_str())
+                });
+            }
         }
     }
 }
